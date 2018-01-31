ed7788b0ea38ff7e50abfefd09efd88f0fa74c11
Mitch Harris <mitchell.harris@gpsit.co.nz>
2018-01-31 15:07:20 +1300

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# GPS-it Developer Candidate Test
+
+This project is designed to allow you to show off your JS developing skills. Provided is a fresh out of the box create-react-app, with the **[Esri JS API](https://developers.arcgis.com/javascript/latest/api-reference/index.html)** and map already loaded and ready to go. Use these **[wire frames](https://gpsitnz.sharepoint.com/:b:/g/EY1MQXXBpNhDsd_M8kRvNDcBwsNlySmIjeu2FlEuKv9B_w?e=EUBPUU)** to create a web application to the best of your ability! We are interested in:
+
+  - Standard coding style across the entire project.
+  - Unit testing.
+  - Useful comments.
+  - Problem solving skills.
+  - Maybe a little bit of black magic.
+
+We expect you should be able to get this boiler plate up and running without any issue! If you have any questions about the wire frames please feel free to ask, and remember Have fun!!! 🤘🏻
---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import EsriLoaderReact from 'esri-loader-react';
+import './App.css';
+
+class App extends Component {
+  
+  onMapReady = ({ loadedModules, containerNode }) => {
+
+    // Extract the loaded api modules
+    const [
+      WebMap,
+      MapView
+    ] = loadedModules;
+
+    // Creates the web map.
+    // This will likely be your starting point! You will need to know about this class
+    // as it will contain what you need to complete the task!
+    const webMap = new WebMap({
+      portalItem: {
+        id: 'c3337bfc8f964688856b36a4651b66cf'
+      }
+    });
+
+    // Loads the web map into the map view
+    // This just displays the data and map on the screen! :)
+    const mapView = new MapView({
+      container: containerNode,
+      map: webMap
+    });
+  }
+  
+  render() {
+    
+    return (
+      <div className="app-container">
+
+        {/* Load esri API and the map */}
+        <EsriLoaderReact 
+          options={{ url: 'https://js.arcgis.com/4.6/' }}
+          modulesToLoad={[
+            'esri/WebMap',
+            'esri/views/MapView'
+          ]}    
+          onReady={this.onMapReady}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
