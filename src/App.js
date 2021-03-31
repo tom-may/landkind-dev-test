8145c9eb30c6c326c7745f6c0ce564d37d3e6703
alexdunham <alexandra.dunham@gpsit.co.nz>
2021-04-01 06:56:36 +1300

    Updated versions

--- src/App.js
+++ src/App.js
@@ -3,15 +3,12 @@ import EsriLoaderReact from 'esri-loader-react';
 import './App.css';
 
 class App extends Component {
-  
   onMapReady = ({ loadedModules, containerNode }) => {
-
     // Extract the loaded api modules
     const [
       WebMap,
       MapView
     ] = loadedModules;
-
     // Creates the web map.
     // This will likely be your starting point! You will need to know about this class
     // as it will contain what you need to complete the task!
@@ -20,7 +17,6 @@ class App extends Component {
         id: 'c3337bfc8f964688856b36a4651b66cf'
       }
     });
-
     // Loads the web map into the map view
     // This just displays the data and map on the screen! :)
     const mapView = new MapView({
@@ -33,10 +29,9 @@ class App extends Component {
     
     return (
       <div className="app-container">
-
         {/* Load esri API and the map */}
         <EsriLoaderReact 
-          options={{ url: 'https://js.arcgis.com/4.6/' }}
+          options={{ url: 'https://js.arcgis.com/4.18/' }}
           modulesToLoad={[
             'esri/WebMap',
             'esri/views/MapView'
