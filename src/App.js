78881e6f9674e09ed64de90fc26e76db2cc81d5b
alexdunham <alexandra.dunham@gpsit.co.nz>
2022-03-31 14:00:17 +1300

    Updated test to latest esri version

--- src/App.js
+++ src/App.js
@@ -1,46 +1,29 @@
-import React, { Component } from 'react';
-import EsriLoaderReact from 'esri-loader-react';
+import React, { useEffect } from 'react';
+import WebMap from "@arcgis/core/WebMap";
+import MapView from "@arcgis/core/views/MapView"
 import './App.css';
 
-class App extends Component {
-  onMapReady = ({ loadedModules, containerNode }) => {
-    // Extract the loaded api modules
-    const [
-      WebMap,
-      MapView
-    ] = loadedModules;
-    // Creates the web map.
-    // This will likely be your starting point! You will need to know about this class
-    // as it will contain what you need to complete the task!
-    const webMap = new WebMap({
-      portalItem: {
-        id: 'c3337bfc8f964688856b36a4651b66cf'
-      }
-    });
-    // Loads the web map into the map view
-    // This just displays the data and map on the screen! :)
-    const mapView = new MapView({
-      container: containerNode,
-      map: webMap
-    });
-  }
+export default function App() {
+  useEffect(() => {
+    const setupWebMap = () => {
+      const webMap = new WebMap({
+        portalItem: {
+          id: 'c3337bfc8f964688856b36a4651b66cf'
+        }
+      });
+      const mapView = new MapView({
+        container: "mapView",
+        map: webMap
+      });
+    }
+    setupWebMap()
+  }, [])
   
-  render() {
-    
-    return (
-      <div className="app-container">
-        {/* Load esri API and the map */}
-        <EsriLoaderReact 
-          options={{ url: 'https://js.arcgis.com/4.18/' }}
-          modulesToLoad={[
-            'esri/WebMap',
-            'esri/views/MapView'
-          ]}    
-          onReady={this.onMapReady}
-        />
-      </div>
-    );
-  }
+  return (
+    <div className="app-container">
+    {/* Load esri API and the map */}
+    <div id="mapView" className='map-view'/>
+  </div>
+  )
 }
 
-export default App;
